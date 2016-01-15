b4c1d026ddb2e4e6efd839779ce19d0761d57d5d
deagle67 <deagle67@hotmail.com>
2016-01-16 00:29:24 +0100

    premier commit

---
+++ OpenClassRoomsActiviteASP1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OpenClassRoomsActiviteASP1.MvcApplication" Language="C#" %>
---
+++ OpenClassRoomsActiviteASP1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OpenClassRoomsActiviteASP1.Startup))]
+namespace OpenClassRoomsActiviteASP1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
